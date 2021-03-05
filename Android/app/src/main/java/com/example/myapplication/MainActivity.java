c97dd5b608bd33fa49220c083b5cf4391521cf17
zhang.yizhe@northeastern.edu <zhang.yizhe@northeastern.edu>
2021-03-04 19:31:59 -0500

    add android

---
+++ Android/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_main);
+        WebView webView= findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://ervinzhang.pythonanywhere.com/");
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # EGGroup AI    
-React + Flask RESTful  
+React + Flask RESTful + Sqlite  
+  
+## Design  
+WXapp / Android / IOS / Web  
+/login 登录(CSS3动画+人脸识别+语言识别(文字转语音)+Geolocation+Notification+Cookie(token)+Session(DB中))  
+/home 语音识别+翻译  
+/admin 后台管理  
   
 ## Features  
 Speech Recognition  
